067436ab6157a43929ebe3bd337fa3b107d8db40
Carlos Coral <coralcarlos21@gmail.com>
2025-08-23 12:11:58 -0500

    feat(CY-1): R2DBC Configuration

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/test/java/com/carloscoral/r2dbc/UserReactiveRepositoryAdapterTest.java
@@ -0,0 +1,40 @@
+package com.carloscoral.r2dbc;
+
+import com.carloscoral.model.user.User;
+import com.carloscoral.r2dbc.entity.UserEntity;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.reactivecommons.utils.ObjectMapper;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserReactiveRepositoryAdapterTest {
+    @InjectMocks
+    UserReactiveRepositoryAdapter repositoryAdapter;
+
+    @Mock
+    UserReactiveRepository repository;
+
+    @Mock
+    ObjectMapper mapper;
+
+    void mustSaveValue() {
+        User user = new User();
+        UserEntity userEntity = new UserEntity();
+        when(repository.save(userEntity)).thenReturn(Mono.just(userEntity));
+        when(mapper.map(user, User.class)).thenReturn(user);
+
+        Mono<Void> result = repositoryAdapter.saveUser(user);
+
+        // TODO Fix tests
+        StepVerifier.create(result)
+                .expectNextMatches(value -> value.equals("test"))
+                .verifyComplete();
+    }
+}
